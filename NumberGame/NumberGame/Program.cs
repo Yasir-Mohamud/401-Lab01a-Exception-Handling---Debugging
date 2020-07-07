50ed75559ca409cfb19819b21f1ba6b9c8c406cb
yasir mohamud <ymohamed018@gmail.com>
2020-07-06 20:53:51 -0700

    added all files and completed the Populate Method

---
+++ NumberGame/NumberGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StartSequence();
+        }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Enter a number greater than zero");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your answer is  {answer}");
+            int[] inputArr = new int[answer];
+            Populate(inputArr);
+           /* int sum = GetSum(InputArr);
+            int product = GetProduct(InputArr,sum);
+            decimal quotient =  GetQuotient(product);*/
+     
+           
+        }   
+        
+        static int[] Populate(int[] arr)
+        {
+            Console.WriteLine(" The size of your array is {0} ", arr.Length);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Please enter {0} of {1} " , i + 1 , arr.Length);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            return arr;
+        }
+    }
+}
